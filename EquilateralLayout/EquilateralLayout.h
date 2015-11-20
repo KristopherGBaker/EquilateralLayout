689bb6b5689ac9ff1efb7b5dffc7de1edc35880a
Kris Baker <kris.baker@bodybuilding.com>
2015-11-20 01:38:27 -0700

    Added framework project

---
+++ EquilateralLayout/EquilateralLayout.h
@@ -0,0 +1,19 @@
+//
+//  EquilateralLayout.h
+//  EquilateralLayout
+//
+//  Created by Kristopher Baker on 11/20/15.
+//  Copyright © 2015 Kris Baker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EquilateralLayout.
+FOUNDATION_EXPORT double EquilateralLayoutVersionNumber;
+
+//! Project version string for EquilateralLayout.
+FOUNDATION_EXPORT const unsigned char EquilateralLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EquilateralLayout/PublicHeader.h>
+
+
